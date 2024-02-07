f8212e3038c53ed9aedbc15bfdf8e8be282ab46b
hasan7n <hasankassim7@hotmail.com>
2024-02-07 17:04:42 +0100

    fix running medperf server

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /medperf/server
-bash ./setup-dev-server.sh < /dev/null &>server.log &
-sleep 10
+alias python=python3.9 && bash ./setup-dev-server.sh < /dev/null &>server.log &
+sleep 30
 python3.9 ./seed.py --demo benchmark
