c286fe5de6e60ddbc383ba23c5b8bef61f378fb9
hasan7n <hasankassim7@hotmail.com>
2024-02-07 19:59:11 +0100

    fix running server issue

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cd /medperf/server
-alias python=python3.9 && bash -i ./setup-dev-server.sh < /dev/null &>server.log &
+
+cp .env.local.local-auth .env
+# hotfix
+sed -i 's/python /python3.9 /g' setup-dev-server.sh
+bash ./setup-dev-server.sh < /dev/null &>server.log &
 sleep 30
 python3.9 ./seed.py --demo benchmark
