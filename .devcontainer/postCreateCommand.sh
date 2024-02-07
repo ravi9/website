47b12d76c544551047f50d60439a813890d2da4e
hasan7n <hasankassim7@hotmail.com>
2024-02-08 00:50:36 +0100

    fix tar command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 wget https://storage.googleapis.com/medperf-storage/rsna2023/datasets.tar.gz
-tar -xf datasets.tar.gz
+tar -xf datasets.tar.gz -C /workspaces/website/tutorial/
 rm datasets.tar.gz
-mv datasets /workspaces/website/tutorial/datasets
