9531f6bbef893d484e8f453c1011e6ea420d7013
hasan7n <hasankassim7@hotmail.com>
2024-02-08 01:01:22 +0100

    update poststartcommand

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -7,8 +7,12 @@ sed -i 's/python /python3.9 /g' setup-dev-server.sh
 echo "Running a local MedPerf server"
 bash ./setup-dev-server.sh < /dev/null &>server.log &
 sleep 30
-if [ ! -f "db.sqlite3" ]; then
+if [ ! -f ".already_seeded" ]; then
     echo "Setting up the local database"
     python3.9 ./seed.py --demo benchmark
+    touch .already_seeded
 fi
 medperf profile activate local
+
+# pull FL image
+docker pull hasan7/fltest:0.0.0-cpu
