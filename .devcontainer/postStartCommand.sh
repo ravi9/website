d3561710203c08bc08f332e7606711bae5f8d69d
hasan7n <hasankassim7@hotmail.com>
2024-02-09 12:42:25 +0100

    pull needed docker images at startup

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# pull FL image
+# pull images to save time later
+# for FL
 docker pull hasan7/fltest:0.0.0-cpu
+docker pull mlcommons/miccai2023-tutorial-prep:0.0.0
+# for evaluation
+docker pull mlcommons/miccai2023-trained:1.0.0
+docker pull mlcommons/miccai2023-metrics:0.0.0
 
+# run medperf server
 cd /medperf/server
 cp .env.local.local-auth .env
 # hotfix: use python3.9 in place of python
