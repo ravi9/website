eef67dd5c3f131baa4840dd29be1bf0deec8eeb2
hasan7n <hasankassim7@hotmail.com>
2024-02-09 15:32:19 +0100

    switch docker pulls

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -2,8 +2,8 @@
 
 # pull images to save time later
 # for FL
-docker pull hasan7/fltest:0.0.0-cpu
 docker pull mlcommons/miccai2023-tutorial-prep:0.0.0
+docker pull hasan7/fltest:0.0.0-cpu
 # for evaluation
 docker pull mlcommons/miccai2023-trained:1.0.0
 docker pull mlcommons/miccai2023-metrics:0.0.0
